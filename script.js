a2a6458056aee1ac20d02a780adeb5586871729a
YooJihyeonn <92969180+YooJihyeonn@users.noreply.github.com>
2021-12-12 22:29:27 +0900

    product onmousemove 작동 다시

--- script.js
+++ script.js
@@ -35,8 +35,122 @@ const scenseButtonOne = document.querySelector('.cherry');
     console.dir(circleText);
     circleText.style.transform = "rotate(" + window.pageYOffset/2 + "deg)";
     // 웹 브라우저에서 현재 화면의 스크롤 정보를 알아야 할 때 pageYOffset or scrollY : 스크롤 화면 수직 이동하는 픽셀수
-    // 왜 따옴표를 저렇게 썻을까요...? 그리고 pageYOffset을 왜 2로 나누었을까요..?
-    // 저렇게 걸쳐지게 따옴표를 하지 않으면 작동이 되지 않는데 왜그런걸까요..? 
+ 
   }
   window.addEventListener('scroll',rotateImage);
+
+
+  const saDefaltMagin = 300;
+        const saElementList = document.querySelectorAll('.sa');
+        let saTriggerHeight = 0;
+        let saTriggerMagin = 0;
+
+        const saMove =function(){
+          for(const element of saElementList){
+              if(!element.classList.contains('show')){
+                  if(element.dataset.saMargin){
+                      saTriggerMagin = parseInt(element.dataset.saMargin);
+                  }else{
+                      saTriggerMagin = saDefaltMagin;
+                  }
+                  if(element.dataset.saTrigger){
+                      saTriggerHeight = document.querySelector(element.dataset.saTrigger).getBoundingClienRect().top + saTriggerMagin;
+                  }else{
+                      saTriggerHeight = element.getBoundingClientRect().top + saTriggerMagin;
+                  }
+                  if(window.innerHeight > saTriggerHeight){
+                      element.classList.add('show');
+                  }
+              }
+          }
+      }
+      window.addEventListener('load',saMove);
+      window.addEventListener('scroll',saMove);
+
+      // h3에 마우스호버했을 때 h3이 옆으로 이동하면서 
+        // 보이지 않던 이미지가 보이고 마우스 커서를 따라다녔으면 좋겟어
+        // 그리고 정해진 영역에서 벗어나면 다음 영역에 이미지로 바뀌였으면 좋겟어
+        const imagesFadein = document.querySelector('#diffserimg');
+        const container = document.querySelector('.containerdiffser');
+        const textTag = document.querySelector('.diffsertext')
+        console.log(container);
+        console.log(imagesFadein);
+
+        container.addEventListener('mouseenter', function () {
+          imagesFadein.animate([
+              {height:'10vh',width:'10vw',opacity:0},
+              {height:'20vh',width:'20vw',opacity:0.5},
+              {height:'35vh',width:'30vw',opacity:1}
+            ],{duration:500,fill: 'forwards'});
+            imagesFadein.classList.remove('hidden');
+            textTag.style.marginLeft = "25vh";
+        });
+        container.onmousemove = (e) => {
+            // 마우스의 좌표값 clientX,Y 현재 보이는 브라우저 기준 스크롤은 무시하고 해당 페이지의 상단을 0으로 측정
+            const x = e.pageX;
+            const y = e.pageY;
+            imagesFadein.style.left = (x - 250) + "px";
+            imagesFadein.style.top = (y - 140) + "px";
+        };
+        container.addEventListener('mouseout', function () {
+            imagesFadein.classList.add('hidden');
+            textTag.style.marginLeft = "0"
+            textTag.style.color = ""
+        });
+
+        const fabricimg = document.querySelector('#fabricwrap img');
+        const fabircCon = document.querySelector('.containerfabric');
+        const fabricTag = document.querySelector('.fabrictext');
+        console.log(fabricimg);
+        console.log(fabircCon);
+        console.log(fabricTag);
+
+        fabircCon.addEventListener('mouseenter',function(){
+          fabricimg.animate([
+              {height:'10vh',width:'10vw',opacity:0},
+              {height:'20vh',width:'20vw',opacity:0.5},
+              {height:'35vh',width:'30vw',opacity:1}
+            ],{duration:500,fill: 'forwards'});
+          fabricimg.classList.remove('hidden');
+          fabricTag.style.marginRight = "25vh";
+        });
+        fabircCon.onmousemove = (e) => {
+          // pageX,Y 문서 전체를 기준으로 삼음 스크롤 화면 포함해서 측정
+            const x = e.pageX;
+            const y = e.pageY;
+            fabricimg.style.left = (x - 250) + "px";
+            fabricimg.style.top = (y - 140) + "px";
+        };
+        fabircCon.addEventListener('mouseout',function(){
+          fabricimg.classList.add('hidden');
+          fabricTag.style.marginRight = "";
+        });
+        
+        const deoimg = document.querySelector('#deowrap img');
+        const deoCon = document.querySelector('.containerdeo');
+        const deoTag = document.querySelector('.deotext');
+        console.log(deoimg);
+        console.log(deoCon);
+        console.log(deoTag);
+        
+        deoCon.addEventListener('mouseenter',function(){
+          deoimg.animate([
+              {height:'10vh',width:'10vw',opacity:0},
+              {height:'20vh',width:'20vw',opacity:0.5},
+              {height:'35vh',width:'30vw',opacity:1}
+            ],{duration:500,fill: 'forwards'});
+          deoimg.classList.remove('hidden');
+          deoTag.style.marginLeft = "25vh";
+        });
+        deoCon.onmousemove = (e) => {
+          // pageX,Y 문서 전체를 기준으로 삼음 스크롤 화면 포함해서 측정
+            const x = e.pageX;
+            const y = e.pageY;
+            deoimg.style.left = (x - 250) + "px";
+            deoimg.style.top = (y - 140) + "px";
+        };
+        deoCon.addEventListener('mouseout',function(){
+          deoimg.classList.add('hidden');
+          deoTag.style.marginLeft = "";
+        });
 
